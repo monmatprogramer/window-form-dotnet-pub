f41e726c2cb43a5d8d17f8a7af07362aca3c8ae2
monmatprogrammer <monsokmeanmon@gmail.com>
2025-05-26 08:58:46 +0700

    Test custom form

--- window-form-dotnet/Program.cs
+++ window-form-dotnet/Program.cs
@@ -11,7 +11,7 @@ namespace window_form_dotnet
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new NewCustomerForm());
         }
     }
 }
